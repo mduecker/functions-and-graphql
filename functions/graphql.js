96e02e0b76c987a5db78dc87a6779cb32bebaa19
Markus Dücker <markus.duecker@student.hpi.de>
2019-08-19 16:59:11 +0200

    Test GraphQL resolver with fetch

--- functions/graphql.js
+++ functions/graphql.js
@@ -1,22 +1,33 @@
-const { ApolloServer, gql } = require('apollo-server-lambda')
+const { ApolloServer, gql } = require("apollo-server-lambda");
 
 const typeDefs = gql`
   type Query {
     hello: String
+    todo: TodoItem!
   }
-`
+
+  type TodoItem {
+    id: ID!
+    userId: Int!
+    title: String!
+    completed: Boolean!
+  }
+`;
 
 const resolvers = {
   Query: {
     hello: (root, args, context) => {
-      return `Hello from Netlify function. https://bit.ly/2UXh0fD`
+      return `Hello from Netlify function. https://bit.ly/2UXh0fD`;
+    },
+    todo: async (root, args, context) => {
+      return await fetch("https://jsonplaceholder.typicode.com/todos/1").json();
     }
   }
-}
+};
 
 const server = new ApolloServer({
   typeDefs,
   resolvers
-})
+});
 
-exports.handler = server.createHandler()
+exports.handler = server.createHandler();
